a276edeb8bf50e95a54239ec1c7488dccdfddf41
UxiT <37305809+UxiT@users.noreply.github.com>
2019-05-11 17:02:03 +0300

    Update bot.py

--- msc_students/bot/bot.py
+++ msc_students/bot/bot.py
@@ -9,13 +9,20 @@ vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
 
-def get_members(chat_id):  # #Возвращает список словарей вида [{},{},{}], гле {} словарь сданными о пользователе: id, first_name, last_name, is_closed, 'can_access_closed'
+def get_members(chat_id):
 
     data = vk.messages.getConversationMembers(
         peer_id=2000000000 + chat_id,
         fields=['id', 'first_name', 'last_name']
     )
     return data['profiles']
+
+
+"""
+Возвращает список словарей вида [{},{},{}], где {} словарь сданными о пользователе:
+ id, first_name, last_name, is_closed, 'can_access_closed'
+"""
+
 
 
 def add_user(chat_id, user_id):  # #Добавляет пользователя user_id в беседу chat_id(id для бота)
@@ -48,3 +55,35 @@ def control(request):
 Но перед этим нужно добавить функцию control в urls.py 
     
 """
+def total_chat_list(user_id, *chat_ids):
+    chats = []
+    for chat_id in chat_ids:
+        chat_data=vk.messages.getConversationMembers(
+            chat_id=chat_id
+        )
+        count = chat_data['count']
+        for i in range(count):
+            if user_id in chat_data['profiles'][i]['id']:
+                chats.append(chat_id)
+    return chats
+
+
+"""
+Функция total_chat_list возвращает списко бесед пользователя user_id в которых он состоит
+из набора *chat_ids
+chat_ids — id бесед, по которым идёт поиск (если удобнее - можно передать список)
+"""
+
+
+def total_users(*chat_ids):
+    users = []
+    for chat_id in chat_ids:
+        data = vk.messages.getConversationMembers(chat_id=chat_id)
+        for i in range(data['count']):
+            try:
+                users.append(dict(id=data['profiles'][i]['id'],
+                                  first_naem=data['profiles'][i]['first_name'],
+                                  last_name=data['profiles'][i]['last_name']))
+            except IndexError:
+                pass
+    return users
